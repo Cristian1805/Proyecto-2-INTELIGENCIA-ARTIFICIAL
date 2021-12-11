343fd8fdee483c8d009a3b954fc1f9dff03bddcb
sebastianbarona <juancaba229@gmail.com>
2021-12-11 11:04:44 -0500

    modificacion cpu

---
+++ Proyecto-2-IA-main/main.py
@@ -0,0 +1,361 @@
+#Autores:
+#Cristian Camilo Lozano 
+#Manuel Perdomo 
+
+from tkinter import *
+from typing import Match 
+import numpy as np
+import random
+import time
+
+size_of_board = 600
+number_of_dots = 6
+symbol_size = (size_of_board / 3 - size_of_board / 8) / 2
+symbol_thickness = 50
+dot_color = '#000000'  
+player1_color = '#0492CF'
+player1_color_light = '#67B0CF'
+player2_color = '#EE4035'
+player2_color_light = '#EE7E77'
+Green_color = '#7BC043'
+dot_width = 0.25*size_of_board/number_of_dots
+edge_width = 0.1*size_of_board/number_of_dots
+distance_between_dots = size_of_board / (number_of_dots)
+
+class CuerdasyCorrales():
+    # ------------------------------------------------------------------
+    # Initialization functions
+    # ------------------------------------------------------------------
+    def __init__(self):
+        self.window = Tk()
+        self.window.title('Cuerdas y Corrales')
+        self.canvas = Canvas(self.window, width=size_of_board, height=size_of_board)
+        self.canvas.pack()
+        self.window.bind('<Button-1>', self.click)
+        self.player1_starts = True
+        self.player2_starts = False
+        self.refresh_board()
+        self.play_again()
+
+    def play_again(self):
+        self.refresh_board()
+        self.board_status = np.zeros(shape=(number_of_dots - 1, number_of_dots - 1))
+        print(self.board_status)
+    
+        self.row_status = np.zeros(shape=(number_of_dots, number_of_dots - 1))
+        self.col_status = np.zeros(shape=(number_of_dots - 1, number_of_dots))
+        
+        # Input from user in form of clicks
+        self.player1_starts = not self.player1_starts
+        self.player1_turn = not self.player1_starts
+#------------------No Borrar XD es mio
+        self.player2_starts = not self.player2_starts
+        self.player2_turn = not self.player2_starts
+#---------------
+        self.reset_board = False
+        self.turntext_handle = []
+
+        self.already_marked_boxes = []
+        self.display_turn_text()
+
+    def mainloop(self):
+        self.window.mainloop()
+
+    # ------------------------------------------------------------------
+    # Logical Functions:
+    # The modules required to carry out game logic
+    # ------------------------------------------------------------------
+
+
+#______________________________________________________________________________________
+    def is_grid_occupiedcpu(self, logical_position, type):
+        
+        print("Valida si la linea esta o no ocupada ya ")
+    
+        r = logical_position[0]
+        c = logical_position[1]
+        occupied = True
+
+        if type == 'row' and self.row_status[c][r] == 0:
+            occupied = False
+            print("Fila desocupada")
+            
+        if type == 'col' and self.col_status[c][r] == 0:
+            occupied = False
+            print("Columna desocupada")
+        
+        else: 
+            print("FIla Ocupada")
+            
+            
+        return occupied
+#______________________________________________________________________________________
+
+
+    def is_grid_occupied(self, logical_position, type):
+        
+        print("Valida si la linea esta o no ocupada ya ")
+    
+        r = logical_position[0]
+        c = logical_position[1]
+        occupied = True
+
+        if type == 'row' and self.row_status[c][r] == 0:
+            occupied = False
+            print("Fila desocupada")
+            
+        if type == 'col' and self.col_status[c][r] == 0:
+            occupied = False
+            print("Columna desocupada")
+        
+        else: 
+            print("FIla Ocupada")
+            
+        return occupied
+
+    def convert_grid_to_logical_position(self, grid_position):
+        grid_position = np.array(grid_position)
+        print("Posicion Coordenadas click: "+str( grid_position) )
+        
+        position = (grid_position-distance_between_dots/4)//(distance_between_dots/2)
+
+        type = False
+        logical_position = []
+        if position[1] % 2 == 0 and (position[0] - 1) % 2 == 0:
+            r = int((position[0]-1)//2)
+            c = int(position[1]//2)
+            logical_position = [r, c]
+            type = 'row'
+            
+            print("Pinta Una linea horizontal")
+            
+            # self.row_status[c][r]=1
+            
+        elif position[0] % 2 == 0 and (position[1] - 1) % 2 == 0:
+            c = int((position[1] - 1) // 2)
+            r = int(position[0] // 2)
+            logical_position = [r, c]
+            type = 'col'
+
+            print("Pinta Una linea vertical")
+            
+            print("Logical Position" + str(logical_position))
+            
+        return logical_position, type
+
+    def mark_box(self):
+        boxes = np.argwhere(self.board_status == -4)
+        
+        print("Cajas Completas")
+        print(boxes)
+        for box in boxes:
+            print (box)
+            if list(box) not in self.already_marked_boxes and list(box) !=[]:
+                self.already_marked_boxes.append(list(box))
+                color = player1_color_light
+                self.shade_box(box, color)
+
+        boxes = np.argwhere(self.board_status == 4)
+        for box in boxes:
+            if list(box) not in self.already_marked_boxes and list(box) !=[]:
+                self.already_marked_boxes.append(list(box))
+                color = player2_color_light
+                self.shade_box(box, color)
+
+    def update_board(self, type, logical_position):
+        r = logical_position[0]
+        c = logical_position[1]
+        
+        print("c=="+ str(c))
+        print("r=="+ str(r))
+        
+        
+        val = 1
+        if self.player1_turn:
+            val =- 1
+
+        if c < (number_of_dots-1) and r < (number_of_dots-1): #number_of_dots=6
+            self.board_status[c][r] += val
+
+        if type == 'row':
+            self.row_status[c][r] = 1
+            if c >= 1:
+                self.board_status[c-1][r] += val
+
+        elif type == 'col':
+            self.col_status[c][r] = 1
+            if r >= 1:
+                self.board_status[c][r-1] += val
+
+    def is_gameover(self):
+        return (self.row_status == 1).all() and (self.col_status == 1).all()
+
+    # ------------------------------------------------------------------
+    # Drawing Functions:
+    # The modules required to draw required game based object on canvas
+    # ------------------------------------------------------------------
+
+    def make_edge(self, type, logical_position):
+        if type == 'row':
+            start_x = distance_between_dots/2 + logical_position[0]*distance_between_dots
+            end_x = start_x+distance_between_dots
+            start_y = distance_between_dots/2 + logical_position[1]*distance_between_dots
+            end_y = start_y
+        elif type == 'col':
+            start_y = distance_between_dots / 2 + logical_position[1] * distance_between_dots
+            end_y = start_y + distance_between_dots
+            start_x = distance_between_dots / 2 + logical_position[0] * distance_between_dots
+            end_x = start_x
+
+        if self.player1_turn:
+            color = player1_color
+        else:
+            color = player2_color
+        self.canvas.create_line(start_x, start_y, end_x, end_y, fill=color, width=edge_width)
+
+    def display_gameover(self):
+        player1_score = len(np.argwhere(self.board_status == -4))
+        player2_score = len(np.argwhere(self.board_status == 4))
+
+        if player1_score > player2_score:
+            # Player 1 wins
+            text = 'Winner: Player 1 '
+            color = player1_color
+        elif player2_score > player1_score:
+            text = 'Winner: Player 2 '
+            color = player2_color
+        else:
+            text = 'Its a tie'
+            color = 'gray'
+
+        self.canvas.delete("all")
+        self.canvas.create_text(size_of_board / 2, size_of_board / 3, font="cmr 60 bold", fill=color, text=text)
+
+        score_text = 'Scores \n'
+        self.canvas.create_text(size_of_board / 2, 5 * size_of_board / 8, font="cmr 40 bold", fill=Green_color,
+                                text=score_text)
+
+        score_text = 'Player 1 : ' + str(player1_score) + '\n'
+        score_text += 'Player 2 : ' + str(player2_score) + '\n'
+        # score_text += 'Tie                    : ' + str(self.tie_score)
+        self.canvas.create_text(size_of_board / 2, 3 * size_of_board / 4, font="cmr 30 bold", fill=Green_color,
+                                text=score_text)
+        self.reset_board = True
+
+        score_text = 'Click to play again \n'
+        self.canvas.create_text(size_of_board / 2, 15 * size_of_board / 16, font="cmr 20 bold", fill="gray",
+                                text=score_text)
+
+    def refresh_board(self):
+        for i in range(number_of_dots):
+            x = i*distance_between_dots+distance_between_dots/2
+            self.canvas.create_line(x, distance_between_dots/2, x,
+                                    size_of_board-distance_between_dots/2,
+                                    fill='gray', dash = (2, 2))
+            self.canvas.create_line(distance_between_dots/2, x,
+                                    size_of_board-distance_between_dots/2, x,
+                                    fill='gray', dash=(2, 2))
+
+        for i in range(number_of_dots):
+            for j in range(number_of_dots):
+                start_x = i*distance_between_dots+distance_between_dots/2
+                end_x = j*distance_between_dots+distance_between_dots/2
+                self.canvas.create_oval(start_x-dot_width/2, end_x-dot_width/2, start_x+dot_width/2,
+                                        end_x+dot_width/2, fill=dot_color,
+                                        outline=dot_color)
+
+    def display_turn_text(self):
+        text = 'Turno del : '
+        if self.player1_turn:
+            text += 'Player1'
+            color = player1_color
+        if self.player2_turn:
+            text += 'Player2'
+            color = player2_color
+        else:
+            text += 'Empieza EL juego'
+            color = '141414'
+        
+
+        self.canvas.delete(self.turntext_handle)
+        self.turntext_handle = self.canvas.create_text(size_of_board - 5*len(text),
+                                                       size_of_board-distance_between_dots/8,
+                                                       font="cmr 15 bold", text=text, fill=color)
+
+
+    def shade_box(self, box, color):
+        start_x = distance_between_dots / 2 + box[1] * distance_between_dots + edge_width/2
+        start_y = distance_between_dots / 2 + box[0] * distance_between_dots + edge_width/2
+        end_x = start_x + distance_between_dots - edge_width
+        end_y = start_y + distance_between_dots - edge_width
+        self.canvas.create_rectangle(start_x, start_y, end_x, end_y, fill=color, outline='')
+
+    def display_turn_text(self):
+        text = 'Turno del : '  #Cambio de turnos 
+        if self.player1_turn:
+            text += 'Player1'
+            color = player1_color 
+            
+        else:
+            text += 'Player2'
+            color = player2_color
+            #no me lo borren :V creo q desde aqui tocaria hacer el llamado al metodo cpu ya que aqui es cuando cambia jugador
+            self.cpu() 
+            
+        self.canvas.delete(self.turntext_handle)
+        self.turntext_handle = self.canvas.create_text(size_of_board - 5*len(text),
+                                                       size_of_board-distance_between_dots/8,
+                                                       font="cmr 15 bold",text=text, fill=color)
+
+    def click(self, event):
+        if not self.reset_board:
+            print("X===,Y==")
+            print([event.x,event.y])
+            grid_position = [event.x, event.y]
+            logical_positon, valid_input = self.convert_grid_to_logical_position(grid_position)
+            if valid_input and not self.is_grid_occupied(logical_positon, valid_input):
+                self.update_board(valid_input, logical_positon)
+                self.make_edge(valid_input, logical_positon)
+                self.mark_box()
+                self.refresh_board()
+                self.player1_turn = not self.player1_turn
+
+                if self.is_gameover():
+                    # self.canvas.delete("all")
+                    self.display_gameover()
+                else:
+                    self.display_turn_text()
+        else:
+            self.canvas.delete("all")
+            self.play_again()
+            self.reset_board = False
+
+    def cpu(self):
+        print("CPUUUUUU")
+        x=random.randint(5,50)*10
+        y=random.randint(5,50)*10
+        if not self.reset_board:
+            grid_position = [x, y]
+            logical_positon, valid_input = self.convert_grid_to_logical_position(grid_position)
+            if valid_input and not self.is_grid_occupiedcpu(logical_positon, valid_input):
+                self.update_board(valid_input, logical_positon)
+                self.make_edge(valid_input, logical_positon)
+                self.mark_box()
+                self.refresh_board()
+                self.player1_turn = not self.player1_turn
+                if self.is_gameover():
+                    # self.canvas.delete("all")
+                    self.display_gameover()
+                else:
+                    self.display_turn_text()
+            else:
+                print("espacio ocupado")
+        else:
+            self.canvas.delete("all")
+            self.play_again()
+            self.reset_board = False      
+
+game_instance = CuerdasyCorrales()
+game_instance.mainloop() 
+
+  
